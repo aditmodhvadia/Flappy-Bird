ce7edd1ab9074f74088782bcc03b3018cd5ade2e
Adit Modhvadia <adit.modhvadia@gmail.com>
2019-01-29 02:04:44 +0530

    - add splash activity

---
+++ android/src/com/aditmodhvadia/flappybird/SplashActivity.java
@@ -0,0 +1,24 @@
+package com.aditmodhvadia.flappybird;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+        final Handler handler = new Handler();
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                startActivity(new Intent(SplashActivity.this, AndroidLauncher.class));
+                finishAffinity();
+            }
+        }, 2000);
+    }
+}
